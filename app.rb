1cd6ba0dbf9ebf0b30b0606dcdadaa6293cac636
Loma <loma.tiyabhorn@gmail.com>
2020-05-20 21:53:35 +0000

    fixed app.rb

--- app.rb
+++ app.rb
@@ -1,7 +1,6 @@
 require "sinatra"
 require "sinatra/reloader"
 require "httparty"
-require 'news-api'
 def view(template); erb template.to_sym; end
 
 get "/" do
@@ -10,20 +9,39 @@ get "/" do
   lat = 42.0574063
   long = -87.6722787
 
-  units = "imperial" # or metric, whatever you like
-  key = "eae067a11018332286c1cae364b5756e" # replace this with your real OpenWeather API key
+  units = "imperial"
+  key = "bf5db749a33df8a2d1ed89fb1a48b53b"
 
   # construct the URL to get the API data (https://openweathermap.org/api/one-call-api)
-  url = "https://api.openweathermap.org/data/2.5/onecall?lat=42.0574063&lon=-87.6722787&units=imperial&appid=eae067a11018332286c1cae364b5756e"
-
-  # make the call
+  url = "https://api.openweathermap.org/data/2.5/onecall?lat=#{lat}&lon=#{long}&units=#{units}&appid=#{key}"
   @forecast = HTTParty.get(url).parsed_response.to_hash
-  
-  ### Get the news
-    # do everything else
+    
+    @current = ["Temperature: #{@forecast["current"]["temp"]} degrees", "Conditions: #{@forecast["current"]["weather"][0]["description"]}"]
+    puts @current
+    puts "8 Day Extended forecast:"
+    extended = []
+    day_number = 1
+    for day in @forecast["daily"]
+        extended << "#{day_number} Days Out: #{day["weather"][0]["description"]} with a high of #{day["temp"]["max"]} degrees and a low of #{day["temp"]["min"]} degrees"
+        day_number = day_number + 1
+    end
 
-  url = "https://newsapi.org/v2/top-headlines?country=us&apiKey=f6dfd183082d4ec6b2e4181882398a51"
+    @extfcast = extended[0, 7]
+
+  ### Get the news
+
+  newskey = "f6dfd183082d4ec6b2e4181882398a51"
+  url = "https://newsapi.org/v2/top-headlines?country=us&apiKey=#{newskey}"
   @news = HTTParty.get(url).parsed_response.to_hash
-  # news is now a Hash you can pretty print (pp) and parse for your output
-  view "weather"
+
+    stories = []
+    article_number = 1
+    for article in @news["articles"]
+        stories << "#{article["url"]}>#{article_number}: #{article["title"]}" 
+        article_number = article_number + 1
+    end
+
+    @topstories = stories[0,10]
+
+    view "news"
 end
