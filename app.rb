43fa4f92e74fa6a3da1125c71920c7260a03cfb4
Loma <loma.tiyabhorn@gmail.com>
2020-05-20 23:08:01 +0000

    edit

--- app.rb
+++ app.rb
@@ -16,13 +16,13 @@ get "/" do
   url = "https://api.openweathermap.org/data/2.5/onecall?lat=42.0574063&lon=-87.6722787&units=imperial&appid=eae067a11018332286c1cae364b5756e"
   @forecast = HTTParty.get(url).parsed_response.to_hash
     
-    @current = ["Temperature: #{@forecast["current"]["temp"]} degrees", "Conditions: #{@forecast["current"]["weather"][0]["description"]}"]
+    @current = ["Temperature: #{@forecast["current"]["temp"]} degrees", "Sky: #{@forecast["current"]["weather"][0]["description"]}"]
     puts @current
     puts "Weekly Forecast:"
     extended = []
     day_number = 1
     for day in @forecast["daily"]
-        extended << "#{day_number} Day: #{day["weather"][0]["description"]} with a Max temperature of #{day["temp"]["max"]} F and a Min temperature of #{day["temp"]["min"]} F"
+        extended << "#{day_number} Day: Max temperature of #{day["temp"]["max"]} F and a Min temperature of #{day["temp"]["min"]} F with #{day["weather"][0]["description"]}"
         day_number = day_number + 1
 
     end
@@ -43,7 +43,7 @@ get "/" do
     for article in @news["articles"]
         stories << "#{article["url"]}>#{article_number}: #{article["title"]}" 
         article_number = article_number + 1
-        
+
     end
 
     @topstories = stories[0,5]
