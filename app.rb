ba7d6f6665143052e840ff05dc739ceda56bd39b
Loma <loma.tiyabhorn@gmail.com>
2020-05-20 19:23:08 +0000

    constructed weather url

--- app.rb
+++ app.rb
@@ -10,10 +10,10 @@ get "/" do
   long = -87.6722787
 
   units = "imperial" # or metric, whatever you like
-  key = "YOUR-API-KEY-GOES-HERE" # replace this with your real OpenWeather API key
+  key = "eae067a11018332286c1cae364b5756e" # replace this with your real OpenWeather API key
 
   # construct the URL to get the API data (https://openweathermap.org/api/one-call-api)
-  url = "https://api.openweathermap.org/data/2.5/onecall?lat=#{lat}&lon=#{long}&units=#{units}&appid=#{key}"
+  url = "https://api.openweathermap.org/data/2.5/onecall?lat=42.0574063&lon=-87.6722787&units=imperial&appid=eae067a11018332286c1cae364b5756e"
 
   # make the call
   @forecast = HTTParty.get(url).parsed_response.to_hash
